01b0263113462623efb4018b78f606224a05f678
dragan.ribes <dragan.ribes@epita.fr>
2021-01-24 20:59:32 +0100

    final commit

--- Reader.py
+++ Reader.py
@@ -10,9 +10,16 @@ class Reader(object):
     """
     def read(self, string):
         sliced_str = string.split()
-        if sliced_str[0] in self.cmds:
-            print(sliced_str)
-            print(self.cmds[sliced_str[0]])
+        if len(sliced_str) != 0 and sliced_str[0] in self.cmds:
+            args_need = sliced_str[1:self.cmds[sliced_str[0]][1]]
+            args_supp = sliced_str[self.cmds[sliced_str[0]][1]+1:]
+            if not self.cmds[sliced_str[0]][2]:
+                return self.cmds[sliced_str[0]][0](args_need,args_supp)
+            else :
+                txt = ""
+                for x in args_supp:
+                    txt += " " + x
+                return self.cmds[sliced_str[0]][0](args_need,txt[1:])
         elif self.not_in_cmds != "":
             print(self.not_in_cmds)
             return self.not_in_cmds
